2a8d87c06ba64617eef768cea3200f09d96fd0e3
Adrian Durran <adrian@intellidroid.eu>
2018-07-29 14:59:20 +0100

    added guessed words table

--- jotto/src/GuessedWords.js
+++ jotto/src/GuessedWords.js
@@ -5,7 +5,28 @@ const GuessedWords = (props) => {
   let contents;
   if (props.guessedWords.length === 0) {
     contents = (
-      <span data-test="guess-instructions">Try to guess the secret word</span>
+      <span data-test="guess-instructions">Try to guess the secret word!</span>
+    );
+  } else {
+    const guessedWordsRows = props.guessedWords.map((word, index) => (
+      <tr key={index} data-test="guessed-word">
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ));
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letter</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRows}</tbody>
+        </table>
+      </div>
     );
   }
 
