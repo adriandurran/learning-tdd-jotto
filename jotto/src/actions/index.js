f174cd72714dbfea16ce755a48db1237ea1e3d16
adriandurran <adriandurran@users.noreply.github.com>
2018-08-03 08:52:40 +0100

    fr

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -1,7 +1,12 @@
 export const actionTypes = {
-  CORRECT_GUESS: 'correct_guess'
+  CORRECT_GUESS: 'correct_guess',
+  GUESS_WORD: 'guess_word',
 };
 
-export const correctGuess = () => {
-  return { type: actionTypes.CORRECT_GUESS };
+// export const correctGuess = () => {
+//   return { type: actionTypes.CORRECT_GUESS };
+// };
+
+export const guessWord = (guessedWord) => {
+  return function(dispatch, getState) {};
 };
