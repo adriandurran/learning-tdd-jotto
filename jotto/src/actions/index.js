f57e732819aeb2b7461f12d0458c281aac723e10
Adrian Durran <adrian@intellidroid.eu>
2018-08-05 10:57:53 +0100

    finished integration tests

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -1,12 +1,26 @@
 export const actionTypes = {
   CORRECT_GUESS: 'correct_guess',
-  GUESS_WORD: 'guess_word',
+  GUESS_WORD: 'guess_word'
 };
+
+import { getLetterMatchCount } from '../helpers';
 
 // export const correctGuess = () => {
 //   return { type: actionTypes.CORRECT_GUESS };
 // };
 
 export const guessWord = (guessedWord) => {
-  return function(dispatch, getState) {};
+  return function(dispatch, getState) {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount }
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS });
+    }
+  };
 };
--- jotto/src/actions/index.test.js
+++
@@ -1,8 +0,0 @@
-import { correctGuess, actionTypes } from './';
-
-describe('correctGuess', () => {
-  test('returns an action with type "correct_guess', () => {
-    const action = correctGuess();
-    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-  });
-});
